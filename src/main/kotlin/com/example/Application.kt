3c95219e70e0db1db9209e218ec246c3ff2e3363
Rafael <rafaelteicas@gmail.com>
2023-12-28 17:54:45 -0300

    first commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
